ff48a113322a8da600eacd1fe01882f013401c9a
Maksim Muruev <maksim.muruev@gmail.com>
2014-09-17 07:54:24 +0300

    + add time format checking

--- src/main/java/com/company/app/App.java
+++ src/main/java/com/company/app/App.java
@@ -4,6 +4,10 @@ import com.company.app.com.company.app.hello.MessagePrinter;
 import com.company.app.com.company.app.hello.MessageService;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -53,13 +57,46 @@ public class App {
 
     public static void main(String[] args) {
 
-        ApplicationContext context =
+/*        ApplicationContext context =
                 new AnnotationConfigApplicationContext(App.class);
         MessagePrinter printer = context.getBean(MessagePrinter.class);
         printer.printMessage();
 
-//        List<String> testColl  = new ArrayList<>();
-//        testColl.parallelStream().collect(Collectors.toSet());
+        List<String> testColl  = new ArrayList<>();
+        testColl.add("Test");
+        testColl.parallelStream().collect(Collectors.toSet());
+        String[] array = testColl.toArray(new String[testColl.size()]);*/
+        DateTime dt = new DateTime();
+        DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM hh:mm a").withZone(DateTimeZone.forID("Africa/Kampala"));
+        String str = fmt.print(dt);
+        System.out.println("Time: " + str);
+
+
+/*        Object lock = new Object ();
+        MyThread mt1 = new MyThread (lock);
+        mt1.setName ("A");
+        MyThread mt2 = new MyThread (lock);
+        mt2.setName ("B");
+        MyThread mt3 = new MyThread (lock);
+        mt3.setName ("C");
+        mt1.start ();
+        mt2.start ();
+        mt3.start ();
+        System.out.println ("main thread sleeping");
+        try
+        {
+           Thread.sleep (3000);
+        }
+        catch (InterruptedException e)
+        {
+        }
+        System.out.println ("main thread awake");
+        synchronized (lock)
+        {
+           lock.notify ();
+        }*/
+
+
 //
 /*        Scanner scanner = new Scanner(System.in);
         System.out.println("Scanner a waiting");
@@ -101,5 +138,52 @@ public class App {
         System.out.println("DummyString: " + ptString + " " + ptString.getActualTypeArguments()[0]);
 
     }*/
+}
+
+// SynchronizationDemo2.java
+class SynchronizationDemo2
+{
+/*   public static void main (String [] args)
+   {
+      FinTrans ft = new FinTrans ();
+      TransThread tt1 = new TransThread (ft, "Deposit Thread");
+      TransThread tt2 = new TransThread (ft, "Withdrawal Thread");
+      tt1.start ();
+      tt2.start ();
+   }*/
+}
+class FinTrans
+{
+   private String transName;
+   private double amount;
+  /* synchronized */void update (String transName, double amount)
+   {
+      this.transName = transName;
+      this.amount = amount;
+      System.out.println (this.transName + " " + this.amount);
+   }
+}
+class MyThread extends Thread
+{
+   private Object o;
+   MyThread (Object o)
+   {
+      this.o = o;
+   }
+   public void run ()
+   {
+      synchronized (o)
+      {
+         try
+         {
+             System.out.println (getName () + " before wait");
+             o.wait ();
+             System.out.println (getName () + " after wait");
+         }
+         catch (InterruptedException e)
+         {
+         }
+      }
+   }
 }
 
